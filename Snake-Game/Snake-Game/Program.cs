5e11159f4e5da9e0c3ec7975bb5b169fd3949026
unknown <C:\\Mail>
2016-05-12 21:41:14 +0300

    Созданы конструкторы

--- Snake-Game/Snake-Game/Program.cs
+++ Snake-Game/Snake-Game/Program.cs
@@ -10,8 +10,14 @@ namespace Snake_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello gays!!");
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
             Console.ReadLine();
         }
+
     }
 }
